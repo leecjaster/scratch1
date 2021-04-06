6e60d761cf2388e120889110942cc819cfcbbb1d
leecjaster <35693724+leecjaster@users.noreply.github.com>
2021-04-06 15:19:20 -0500

    counter done

---
+++ test/src/App.js
@@ -0,0 +1,13 @@
+import Counter from "./components/counter.js"
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Counter></Counter>
+      </header>
+    </div>
+  );
+}
+
+export default App;
